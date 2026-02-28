7afec9d292e5c4f0e31af000fc75356181f81bc7
shyaka-yves <shyakayvany@gmail.com>
2026-02-28 17:12:50 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,20 @@ export const metadata: Metadata = {
   title: "La Creola | Kigali Restaurant",
   description:
     "La Creola is an exquisite culinary sanctuary in Kigali, blending African and Asian inspirations in a luxurious setting.",
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+    other: [
+      {
+        rel: "manifest",
+        url: "/site.webmanifest",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
